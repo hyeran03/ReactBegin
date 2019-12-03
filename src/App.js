abdad03844da00cfbe0d0fed1a812290f61119dd
hyeran03 <user@email.mail>
2019-12-03 15:47:39 +0900

    useRef로 컴포넌트 안에 변수 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 import UserList from "./UserList";
 import "./App.css";
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: "one",
+      email: "one@example.com"
+    },
+    {
+      id: 2,
+      username: "two",
+      email: "two@example.com"
+    },
+    {
+      id: 3,
+      username: "three",
+      email: "three@example.com"
+    },
+    {
+      id: 4,
+      username: "four",
+      email: "four@example.com"
+    }
+  ];
+  const nextId = useRef(5);
+  //useRef 는 어떤 변수를 기억하고 싶을때 사용할 수도 있다
+
+  const onCreate = () => {
+    console.log(nextId.current); //5 현재 useRef의 값
+    nextId.current += 1; //+1 바뀌는 값 그러나 컴포넌트가 리렌더링되는건 아니다
+  };
+
   return (
     <>
-      <UserList />
+      <UserList users={users} />
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -8,50 +8,10 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "one",
-      email: "one@example.com"
-    },
-    {
-      id: 2,
-      username: "two",
-      email: "two@example.com"
-    },
-    {
-      id: 3,
-      username: "three",
-      email: "three@example.com"
-    },
-    {
-      id: 4,
-      username: "four",
-      email: "four@example.com"
-    }
-  ];
+function UserList({ users }) {
   return (
-    // <div>
-    //   <div>
-    //     <b>{users[0].username}</b> <span>({users[0].email})</span>
-    //   </div>
-    //   <div>
-    //     <b>{users[1].username}</b> <span>({users[1].email})</span>
-    //   </div>
-    //   <div>
-    //     <b>{users[2].username}</b> <span>({users[2].email})</span>
-    //   </div>
-
-    //   <div>
-    //     <b>{users[3].username}</b> <span>({users[3].email})</span>
-    //   </div>
-    // </div>
     <div>
-      {/* <User user={users[0]} />
-      <User user={users[1]} />
-      <User user={users[2]} /> */}
-      {users.map((user, index) => (
+      {users.map(user => (
         <User user={user} key={user.id} />
       ))}
     </div>
