4e5c2cc70fe3aecd1016223bbca87f4364336146
hyeran03 <user@email.mail>
2019-12-03 16:06:55 +0900

    배열에 항목 추가하기 -spread 함수

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 import "./App.css";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    uername: "",
+    email: ""
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: "one",
@@ -24,17 +37,38 @@ function App() {
       username: "four",
       email: "four@example.com"
     }
-  ];
+  ]);
+
+  // 배열의 불변성을 지키면서 새로운 항목추가
+  // 스프레드(spread) 연산자 사용
+
   const nextId = useRef(5);
-  //useRef 는 어떤 변수를 기억하고 싶을때 사용할 수도 있다
 
   const onCreate = () => {
-    console.log(nextId.current); //5 현재 useRef의 값
-    nextId.current += 1; //+1 바뀌는 값 그러나 컴포넌트가 리렌더링되는건 아니다
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]); //기존의 배열을 가져와 user에 넣고 user에 새항목을 추가시킨다
+    // setUsers(users.concat(user)); -> concat 함수를 사용하여 항목을 추가하는 방법
+    //push를 사용하면 안된다
+    setInputs({
+      username: "",
+      email: ""
+    });
+    console.log(nextId.current);
+    nextId.current += 1;
   };
 
   return (
     <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
       <UserList users={users} />
     </>
   );
