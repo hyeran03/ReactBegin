dfe2c1900a45bbdae610cff7bf54287fc334012f
hyeran03 <user@email.mail>
2019-12-05 15:46:56 +0900

    커스텀 hook 만들어 사용하기- useInputs (useState)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useRef, useCallback, useReducer, useMemo } from "react";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
+import useInputs from "./useInputs";
 import "./App.css";
 
 function conuntActiveUsers(users) {
@@ -8,12 +9,7 @@ function conuntActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
-//사용할 초기상태를 컴포넌트 밖에 만들기
 const initialState = {
-  inputs: {
-    username: "",
-    email: ""
-  },
   users: [
     {
       id: 1,
@@ -44,27 +40,14 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case "CHANGE_INPUT":
-      // 현재 자신이 지니고 있는 상태에서 input안의 특정값을 바꾸어 주도록
-      return {
-        ...state, //불변성을 지키기위함
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value
-        }
-      };
     case "CREATE_USER":
       return {
-        inputs: initialState.inputs, //위에서 initialState를 선언했기에 사용가능
-        users: state.users.concat(action.user) //user배열을 업데이트하는 작업
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
       };
     case "TOGGLE_USER":
       return {
-        ...state, //기존의 상태
-        //users배열을 업데이트 - id값을 가지고 올것임
-        //각 유저에 대하여 비교 - user.id가 action.id(action을 통해 받아온 id)와 같은지
-        //그것이 같다면 최신값을 가지고있는 user의 active를 반전시킨다
-        //같지 않다면 user을 그대로 유지하겟다
+        ...state,
         users: state.users.map(user =>
           user.id === action.id ? { ...user, active: !user.active } : user
         )
@@ -73,7 +56,6 @@ function reducer(state, action) {
       return {
         ...state,
         users: state.users.filter(user => user.id !== action.id)
-        //state를 그대로 가져오고 user을 가져와서 user.id와 action.id를 비교후 같지 않다면 그대로 두고 같다면 없애라
       };
     default:
       throw new Error("Unhandled action");
@@ -83,36 +65,29 @@ function reducer(state, action) {
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const [form, onChange, reset] = useInputs({
+    username: "",
+    email: ""
+  });
+  // useInput에서 내보낸 배열의 순서와 맞춘다(이름을 맞출필요는 없다)
+  const { username, email } = form;
+
   const nextId = useRef(5);
 
-  //비구조 할당을 통해 추출후 props로
   const { users } = state;
-  const { username, email } = state.inputs;
-
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    //name과 value값을 e.target에서 추출
-    //action값
-    dispatch({
-      type: "CHANGE_INPUT",
-      name,
-      value
-      // e.target에서 받아온 name, value값 사용
-    });
-  }, []);
 
   const onCreate = useCallback(() => {
     dispatch({
       type: "CREATE_USER",
       user: {
-        id: nextId.current, //변화된 최신의 값을 가져온다
+        id: nextId.current,
         username,
         email
       }
     });
     nextId.current += 1;
-  }, [username, email]);
-  // 위의 함수에서 기존상태에 의존하는 값인 [username, email]을 넣어준다
+    reset();
+  }, [username, email, reset]);
 
   const onToggle = useCallback(id => {
     dispatch({
@@ -120,7 +95,6 @@ function App() {
       id
     });
   }, []);
-  //디펜던시배열[]이 비여잇다는 것은 함수를 한번만들고 재사용callback할수 잇다는것
 
   const onRemove = useCallback(id => {
     dispatch({
@@ -146,7 +120,3 @@ function App() {
 }
 
 export default App;
-
-//useReduce  vs  useState
-//컴포넌트에서 관리하는 값이 하나거나 단순할 경우 - useState
-//컴포넌트에서 관리하는 값잉 여러개거나 복잡하고 수정이 빈번할 경우 - useReduce
